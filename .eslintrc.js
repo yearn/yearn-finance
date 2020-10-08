082b14d0615c7cea8a198fc081dc1b13dcb428e8
x48 <x48114@protonmail.com>
2020-10-07 22:32:27 -0500

    refactor: disable proptype checking for duration of initial development phase

--- .eslintrc.js
+++ .eslintrc.js
@@ -67,6 +67,7 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react/jsx-closing-tag-location': 0,
     'react/forbid-prop-types': 0,
+    'react/prop-types': 0,
     'react/jsx-first-prop-new-line': [2, 'multiline'],
     'react/jsx-filename-extension': 0,
     'react/jsx-no-target-blank': 0,

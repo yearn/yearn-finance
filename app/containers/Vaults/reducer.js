2dfcb6369ddc8af2c276c34310d77409b237ddfc
x48 <x48114@protonmail.com>
2020-11-09 19:33:40 -0600

    feat: add support for injecting contract updates into vaults state

--- app/containers/Vaults/reducer.js
+++ app/containers/Vaults/reducer.js
@@ -2,25 +2,48 @@ import produce from 'immer';
 import { GOT_CONTRACT_VAR } from 'containers/DrizzleProvider/constants';
 import { VAULTS_LOADED } from './constants';
 
-export const initialState = {};
+export const initialState = {
+  data: [],
+};
 
 /* eslint-disable default-case, no-param-reassign */
+
+const loadContractData = (draft, action) => {
+  switch (action.type) {
+    case GOT_CONTRACT_VAR: {
+      const {
+        name: address,
+        variable: field,
+        contractType,
+        metadata,
+        value,
+      } = action;
+      if (contractType !== 'vaults') {
+        break;
+      }
+      const item = _.find(draft.data, { address });
+      if (!item) {
+        const newItem = { address };
+        draft.data.push(newItem);
+        newItem[field] = _.clone(value);
+        newItem.metadata = metadata;
+        break;
+      }
+      item[field] = value;
+      item.metadata = metadata;
+      draft.data = _.clone(draft.data);
+      break;
+    }
+  }
+};
+
 const appReducer = (state = initialState, action) =>
   produce(state, draft => {
+    loadContractData(draft, action);
     switch (action.type) {
       case VAULTS_LOADED:
         draft.data = action.vaults;
         break;
-      case GOT_CONTRACT_VAR: {
-        const { name: address, variable: field, value } = action;
-        const vault = _.find(draft.data, { address });
-        if (!vault) {
-          break;
-        }
-        vault[field] = value;
-        draft.data = _.clone(draft.data);
-        break;
-      }
     }
   });
 

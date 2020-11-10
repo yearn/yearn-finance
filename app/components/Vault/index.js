1d4b7ba57eb1e5b86da34c291185481d7895d5b0
x48 <x48114@protonmail.com>
2020-11-09 19:45:16 -0600

    refactor: overhaul vault UI

--- app/components/Vault/index.js
+++ app/components/Vault/index.js
@@ -1,43 +1,43 @@
-import React from 'react';
+import React, { useContext, memo } from 'react';
+import { compose } from 'redux';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import AnimatedNumber from 'components/AnimatedNumber';
-import { Accordion } from 'react-bootstrap';
+import Accordion from 'react-bootstrap/Accordion';
+import AccordionContext from 'react-bootstrap/AccordionContext';
 import Card from 'react-bootstrap/Card';
-
-const Wrapper = styled.div`
-  min-height: 70px;
-  display: flex;
-  align-items: center;
-  max-width: 870px;
-  margin: 0 auto;
-  div {
-    overflow: hidden;
-    margin-right: 50px;
-    text-overflow: ellipsis;
-  }
-`;
+import ColumnList from 'components/Vault/columns';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
+import Arrow from 'images/arrow.svg';
+import BigNumber from 'bignumber.js';
+import { abbreviateNumber } from 'utils/string';
+import { selectDevMode } from 'containers/DevMode/selectors';
 
 const IconAndName = styled.div`
   display: flex;
   align-items: center;
-  width: 200px;
-`;
-
-const Icon = styled.img`
-  width: 50px;
-  margin-right: 20px;
-`;
-
-const Apy = styled.div`
-  width: 100px;
-`;
-
-const Strategy = styled.div`
   width: 250px;
 `;
 
-const UserBalance = styled.div`
-  width: 200px;
+const Icon = styled.img`
+  width: 40px;
+  margin-right: 20px;
+`;
+
+const IconName = styled.div`
+  margin-top: 8px;
+`;
+
+const StyledArrow = styled.img`
+  margin-right: 30px;
+  transform: ${props => (props.expanded ? 'rotate(0)' : 'rotate(-180deg)')};
+  transition: transform 0.1s linear;
+`;
+
+const A = styled.a`
+  display: ilflex;
 `;
 
 const truncateApy = apy => {
@@ -49,36 +49,124 @@ const truncateApy = apy => {
   return apyStr;
 };
 
-export default function Vault(props) {
+const ColorButton = withStyles(theme => ({
+  root: {
+    color: theme.palette.getContrastText(purple[500]),
+    fontFamily: 'Calibre Medium',
+    fontSize: '20px',
+    padding: '8px 40px 5px 40px',
+    margin: '20px',
+    marginLeft: '0px',
+    textTransform: 'inherit',
+    backgroundColor: '#0657F9',
+    '&:hover': {
+      backgroundColor: '#0657F9',
+    },
+  },
+}))(Button);
+
+const LinkWrap = props => {
+  const { devMode, children, address } = props;
+  if (!devMode) {
+    return children;
+  }
+  return (
+    <A
+      href={`https://etherscan.io/address/${address}`}
+      target="_blank"
+      onClick={evt => evt.stopPropagation()}
+    >
+      {children}
+    </A>
+  );
+};
+
+const Vault = props => {
   const { vault } = props;
-  const { vaultAlias, vaultIcon, tokenIcon, strategyName, balanceOf } = vault;
+  const {
+    symbolAlias,
+    vaultIcon,
+    tokenIcon,
+    tokenAddress,
+    tokenSymbolAlias,
+    decimals,
+    // token,
+    totalAssets,
+    balance,
+    balanceOf,
+    address,
+  } = vault;
+
+  const devMode = useSelector(selectDevMode());
+  // const { balance, balanceOf } = contractData;
+
+  // const tokenContract = tokens[tokenAddress || token]; // TODO: Make sure both are in checksum format
+  const tokenBalance = '2';
+  // const tokenSymbol = tokenSymbolAlias || _.get(tokenContract, 'symbol');
+  const tokenSymbol = tokenSymbolAlias;
+  const vaultName = symbolAlias || tokenSymbol;
+  const accordionEventKey = address;
+  const currentEventKey = useContext(AccordionContext);
+  const active = currentEventKey === accordionEventKey;
   const apyOneMonthSample = _.get(vault, 'apy.apyOneMonthSample');
   const apy = truncateApy(apyOneMonthSample);
-  const vaultBalanceOf = (balanceOf / 10 ** 18).toFixed(0);
+  const tokenBalanceOf = new BigNumber(tokenBalance)
+    .dividedBy(10 ** decimals)
+    .toFixed();
+  const vaultBalanceOf = new BigNumber(balanceOf)
+    .dividedBy(10 ** decimals)
+    .toFixed();
+  let vaultAssets = balance || totalAssets;
+  vaultAssets = new BigNumber(vaultAssets).dividedBy(10 ** decimals).toFixed(0);
+  vaultAssets = vaultAssets === 'NaN' ? '-' : abbreviateNumber(vaultAssets);
   return (
     <React.Fragment>
-      <Card>
+      <Card className={active && 'active'}>
         <Accordion.Toggle
           as={Card.Header}
           variant="link"
-          eventKey={vault.address}
+          eventKey={accordionEventKey}
         >
-          <Wrapper>
+          <ColumnList>
             <IconAndName>
-              <Icon src={vaultIcon || tokenIcon} />
-              {vaultAlias}
+              <LinkWrap devMode={devMode} address={address}>
+                <Icon src={vaultIcon || tokenIcon} />
+              </LinkWrap>
+              <LinkWrap devMode={devMode} address={address}>
+                <IconName devMode={devMode}>{vaultName}</IconName>
+              </LinkWrap>
             </IconAndName>
-            <Strategy>{strategyName}</Strategy>
-            <Apy>{apy}</Apy>
-            <UserBalance>
+
+            <div>
               <AnimatedNumber value={vaultBalanceOf} />
-            </UserBalance>
-          </Wrapper>
+            </div>
+            <div>{vaultAssets}</div>
+            <div>{apy}</div>
+            <div>
+              <AnimatedNumber value={tokenBalanceOf} />{' '}
+              <LinkWrap devMode={devMode} address={tokenAddress}>
+                {tokenSymbol}
+              </LinkWrap>
+            </div>
+          </ColumnList>
+          <StyledArrow src={Arrow} alt="arrow" expanded={active} />
         </Accordion.Toggle>
-        <Accordion.Collapse eventKey={vault.address}>
-          <Card.Body>Hello! Test</Card.Body>
+        <Accordion.Collapse eventKey={accordionEventKey}>
+          <Card.Body>
+            Hello! Test
+            <Card.Footer className={active && 'active'}>
+              <ColorButton variant="contained" color="primary">
+                Deposit
+              </ColorButton>
+              <ColorButton variant="contained" color="primary">
+                Withdraw
+              </ColorButton>
+            </Card.Footer>
+          </Card.Body>
         </Accordion.Collapse>
       </Card>
     </React.Fragment>
   );
-}
+};
+Vault.whyDidYouRender = true;
+export default compose(memo)(Vault);

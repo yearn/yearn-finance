72eed41e514af4c76e17ab76640e2d90359a3965
x48 <x48114@protonmail.com>
2020-11-09 19:46:06 -0600

    feat: add generic input styles

---
+++ app/components/Input/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.input`
+  height: 30px;
+  font-size: 14px;
+  padding: 0px 7px;
+  width: 350px;
+  border: 2px solid ${props => props.theme.inputBorder};
+  border-radius: 23.5px;
+  background-color: transparent;
+  outline: none;
+  color: ${props => props.theme.text};
+  &:focus {
+    border-color: ${props => props.theme.yearnBlue};
+    box-shadow: 0 0 25pt 5pt ${props => props.theme.inputOutline};
+  }
+  padding: 0px 13px;
+`;
+
+export default function ConnectButton(props) {
+  const { children, ...restProps } = props;
+  return (
+    <Wrapper {...restProps} type="text">
+      {props.children}
+    </Wrapper>
+  );
+}

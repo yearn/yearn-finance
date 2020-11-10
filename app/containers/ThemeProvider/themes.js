5c4b59f8d4f9d222712804fab726af7d59378248
x48 <x48114@protonmail.com>
2020-11-09 19:34:36 -0600

    chore: add more theme constants

--- app/containers/ThemeProvider/themes.js
+++ app/containers/ThemeProvider/themes.js
@@ -1,24 +1,38 @@
-const common = {
+export const common = {
   yearnBlue: '#0151f0',
   white: '#fff',
+  black: '#000',
+  offBlack: '#363537',
+  offWhite: '#D8D8D8',
 };
 
 export const lightTheme = {
-  background: '#FFF',
-  text: '#363537',
-  toggleBorder: '#FFF',
-  vaultBackground: '#FAFAFA',
-  vaultBorder: '#CCC',
+  background: '#fff',
+  text: common.offBlack,
+  toggleBorder: common.white,
+  vaultBackground: '#171D23',
+  vaultBackgroundActive: '#171D23',
+  vaultBorderActive: '#0657F9',
+  vaultText: common.white,
+  inputBorder: common.black,
+  inputText: common.black,
+  inputOutline: 'transparent',
   link: 'blue',
   ...common,
 };
 
 export const darkTheme = {
-  background: '#000',
-  text: '#FAFAFA',
+  background: common.black,
+  text: common.offWhite,
   toggleBorder: '#6B8096',
-  vaultBackground: '#444',
-  vaultBorder: '#888',
+  vaultBackground: '#021A4B',
+  vaultBackgroundActive: '#011130',
+  vaultBackgroundMiddle: '#010919',
+  vaultText: common.offWhite,
+  vaultBorderActive: '#0657F9',
+  inputBorder: common.white,
+  inputText: common.white,
+  inputOutline: common.yearnBlue,
   link: 'antiquewhite',
   ...common,
 };

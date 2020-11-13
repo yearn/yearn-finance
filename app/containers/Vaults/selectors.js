cdd1e67c51787062ee1c4dfa5315de39990bf396
x48 <x48114@protonmail.com>
2020-11-13 12:24:23 -0600

    refactor: remove vaults reducer. utilize 'app > vaults' instead

--- app/containers/Vaults/selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selectApp = state => state.vaults;
-
-export const selectVaults = () =>
-  createSelector(
-    selectApp,
-    substate => substate.data,
-  );

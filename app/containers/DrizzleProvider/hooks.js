0661b8064845cf1b66146757bb2b546cc7431a45
x48 <x48114@protonmail.com>
2020-11-13 19:13:25 -0600

    feat: add "useContract" hook

--- app/containers/DrizzleProvider/hooks.js
+++ app/containers/DrizzleProvider/hooks.js
@@ -5,3 +5,11 @@ export function useDrizzle() {
   const { drizzle } = useContext(context);
   return drizzle;
 }
+
+export function useContract(address) {
+  const { drizzle } = useContext(context);
+  if (!drizzle) {
+    return {};
+  }
+  return drizzle.contracts[address];
+}

c3f6b5e30e74266c447224391a494c8b6374757e
Eric Juta <ericjohnjuta@gmail.com>
2020-12-11 19:36:32 +0000

    feat: check in navbar story

---
+++ .storybook/Layout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../app/assets/styles.css';
+
+const Layout = ({ children }) => {
+  return children;
+};
+
+export default Layout;
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,9 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import Layout from './Layout';
+
+addDecorator(storyFn => <Layout>{storyFn()}</Layout>);
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};

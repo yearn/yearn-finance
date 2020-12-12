621b2b6dc98fe02cec1f8ec425d42c69aabfe1df
Eric Juta <ericjohnjuta@gmail.com>
2020-12-12 21:10:55 +0000

    feat: integrate navbar into dapp

--- app/components/Navbar/index.js
+++ app/components/Navbar/index.js
@@ -1,61 +1,72 @@
 import React, { useRef } from 'react';
 import { useClickAway } from 'react-use';
-import tw, { css } from 'twin.macro';
+import tw from 'twin.macro';
+import ConnectButton from 'components/ConnectButton';
 import { FlyingMobileMenu } from './FlyingMobileMenu';
 import { menuLinks } from './menuLinks';
+import { Logo } from './logo';
 
-const FlyingMenu = ({ isActive, clickAwayRef, links }) => (
-  <div
-    ref={clickAwayRef}
-    css={[
-      isActive ? tw`opacity-100 animate-flyingMenuEntering` : tw`opacity-0`,
-    ]}
-    className="absolute z-10 -ml-6 transform px-2 min-w-max max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2 animate-flyingMenuEntering"
-  >
-    <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-      <div className=" bg-black relative pl-8 pr-36 space-y-4 py-6">
-        {links.map(link => (
-          <a href={link.href} className="flex items-start">
-            <div
-              css={[
-                // amazing
-                css`
-                  &:hover {
-                    > *:not(:last-child) {
-                      ${tw`text-yearn-blue`}
-                    }
-                  }
-                `,
-              ]}
-            >
-              <p className="text-base font-black text-white inline-block mr-2">
-                {link.title}
-              </p>
-              <svg
-                css={[link.href[0] !== '/' && tw`inline-block`]}
-                className="hidden text-white h-4 self-start"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                />
-              </svg>
-              <p className="mt-1 text-sm text-white">{link.description}</p>
-            </div>
-          </a>
-        ))}
-      </div>
-    </div>
-  </div>
-);
+// TODO: Uncomment when nested menus are ready
+// const FlyingMenu = ({ isActive, clickAwayRef, links }) => (
+//   <div
+//     ref={clickAwayRef}
+//     css={[
+//       isActive
+//         ? tw`opacity-100 animate-flyingMenuEntering`
+//         : tw`hidden opacity-0`,
+//     ]}
+//     className="absolute z-10 -ml-6 transform px-2 sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2 animate-flyingMenuEntering"
+//   >
+//     <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+//       <div className=" bg-black relative pl-4 min-w-max max-w-md  pr-4 space-y-4 py-6">
+//         {links.map(link => (
+//           <a href={link.href} className="flex items-start">
+//             <div
+//               css={[
+//                 // amazing
+//                 css`
+//                   ${tw`w-full`};
+//                   &:hover {
+//                     > *:not(:last-child) {
+//                       ${tw`text-yearn-blue`}
+//                     }
+//                   }
+//                 `,
+//               ]}
+//             >
+//               <p className="text-base font-black text-white inline-block mr-2">
+//                 {link.title}
+//               </p>
+//               <svg
+//                 css={[link.href[0] !== '/' && tw`inline-block`]}
+//                 className="hidden text-white h-4 self-start"
+//                 xmlns="http://www.w3.org/2000/svg"
+//                 fill="none"
+//                 viewBox="0 0 24 24"
+//                 stroke="currentColor"
+//               >
+//                 <path
+//                   strokeLinecap="round"
+//                   strokeLinejoin="round"
+//                   strokeWidth={2}
+//                   d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+//                 />
+//               </svg>
+//               <p className="mt-1 text-sm text-white">{link.description}</p>
+//             </div>
+//           </a>
+//         ))}
+//       </div>
+//     </div>
+//   </div>
+// );
 
-const MenuItem = ({ text, isActive, setIsActive, links }) => {
+const MenuItem = ({
+  text,
+  isActive,
+  setIsActive,
+  // ,links
+}) => {
   const ref = useRef(null);
   useClickAway(ref, () => {
     setIsActive(false);
@@ -63,17 +74,18 @@ const MenuItem = ({ text, isActive, setIsActive, links }) => {
 
   return (
     <div className="relative">
-      {/* <!-- Item active: "text-gray-900", Item inactive: "text-gray-500" --> */}
-      <div
-        onClick={() => {
-          setIsActive(text);
-        }}
-        onMouseEnter={() => {
-          setIsActive(text);
-        }}
-        onKeyPress={() => {
-          setIsActive(text);
-        }}
+      <a
+        // TODO: Uncomment when nested menus are ready
+        // onClick={() => {
+        //   setIsActive(text);
+        // }}
+        // onMouseEnter={() => {
+        //   setIsActive(text);
+        // }}
+        // onKeyPress={() => {
+        //   setIsActive(text);
+        // }}
+        href={`/${text}`}
         role="button"
         tabIndex="0"
       >
@@ -83,13 +95,13 @@ const MenuItem = ({ text, isActive, setIsActive, links }) => {
         >
           {text}
         </span>
-      </div>
-
-      <FlyingMenu
+      </a>
+      {/* TODO: Uncomment later when ready */}
+      {/* <FlyingMenu
         clickAwayRef={ref}
         isActive={text === isActive}
         links={links}
-      />
+      /> */}
     </div>
   );
 };
@@ -100,7 +112,7 @@ const Navbar = () => {
   const [mobileIsActive, setMobileIsActive] = React.useState(false);
 
   return (
-    <div className="relative bg-black w-screen">
+    <div className="relative z-20 bg-black w-full">
       <div className="px-4 sm:px-6">
         <div className="flex py-4 justify-between">
           <div className="-mr-2 -my-2 md:hidden">
@@ -131,11 +143,9 @@ const Navbar = () => {
           <div className="flex self-center">
             <a href="/">
               <span className="sr-only">Logo</span>
-              <img
-                className="h-5 w-auto"
-                src="/navbar-logo.inline.svg"
-                alt="logo"
-              />
+              <div className="h-5 w-auto">
+                <Logo />
+              </div>
             </a>
           </div>
 
@@ -161,14 +171,7 @@ const Navbar = () => {
             })}
           </nav>
 
-          <div className="">
-            <a
-              href="/connect"
-              className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-            >
-              Connect
-            </a>
-          </div>
+          <ConnectButton />
         </div>
       </div>
     </div>

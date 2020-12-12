f90d280b058cdbb3bef01bbc33957dfdb02656b1
x48 <x48114@protonmail.com>
2020-12-12 16:49:21 -0600

    fix: fix vault name styling

--- app/components/Vault/columns.js
+++ app/components/Vault/columns.js
@@ -3,15 +3,15 @@ import styled from 'styled-components';
 export default styled.div`
   display: grid;
   grid-template-columns: 270px 160px 140px 200px 1fr;
+  font-family: 'Roboto';
+  font-weight: 900;
+  font-size: 20px;
   width: 100%;
   align-items: center;
   > div {
     overflow: hidden;
     white-space: nowrap;
     text-overflow: ellipsis;
-    &:not(:first-of-type) {
-      margin-top: 8px;
-    }
     &:first-of-type {
       margin-left: 15px;
     }

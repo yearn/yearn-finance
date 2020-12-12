5c3ba5544efc3bf9f3d3c0b1168fc36f87f825c4
x48 <x48114@protonmail.com>
2020-12-12 14:47:22 -0600

    chore: add cover utils placeholder

---
+++ app/utils/cover.js
@@ -0,0 +1,21 @@
+// export const calculateTokenPrice = val => {
+//   const formatter = new Intl.NumberFormat('en-US', {
+//     style: 'currency',
+//     currency: 'USD',
+//     minimumFractionDigits: 4,
+//   });
+//   const newVal = formatter.format(val);
+//   return newVal;
+// };
+
+// function calculatePrice(
+//   daiInPool,
+//   covTokensInPool,
+//   daiPrice,
+//   covTokenWeight,
+//   daiWeight,
+// ) {
+//   return (
+//     daiInPool / ((covTokensInPool * daiWeight) / (covTokenWeight * daiPrice))
+//   );
+// }

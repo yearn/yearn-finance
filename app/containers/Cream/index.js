a31477376946109ba0e23e59cba579b835e7fb8d
Graham <grahamu@gmail.com>
2020-12-14 11:40:16 +0000

    feat: add skeleton table for cream markets, fetch borrowRate and show in UI

--- app/containers/Cream/index.js
+++ app/containers/Cream/index.js
@@ -1,9 +1,12 @@
+import { selectContractData } from 'containers/App/selectors';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { useInjectSaga } from 'utils/injectSaga';
 import { useInjectReducer } from 'utils/injectReducer';
 import reducer from './reducer';
 import saga from './saga';
+import { BLOCKS_PER_YEAR } from './constants';
 
 const Wrapper = styled.div`
   margin: 0 auto;
@@ -11,12 +14,64 @@ const Wrapper = styled.div`
   padding: 50px 40px;
 `;
 
-const Cover = () => {
+const CreamBorrowMarketRow = ({ creamTokenAddress }) => {
+  const creamTokenData = useSelector(selectContractData(creamTokenAddress));
+
+  console.log(creamTokenData);
+
+  const borrowRatePerYear =
+    (creamTokenData.borrowRatePerBlock * BLOCKS_PER_YEAR) / 1e16;
+
+  // (cyWETH) needs to be special-cased so it's APY is incorrect for now see
+  // iearn-finance:src/stores/store.jsx:4197
+
+  return (
+    <tr>
+      <td>Token Name</td>
+      <td>{borrowRatePerYear}</td>
+    </tr>
+  );
+};
+
+const Cream = () => {
   useInjectSaga({ key: 'cream', saga });
   useInjectReducer({ key: 'cream', reducer });
 
-  return <Wrapper>cream </Wrapper>;
+  // Hardcoding cream token address temporarily until added to store.
+  // Actual contracts are syncing and stored, but list of addresses to loop over
+  // are yet to be stored.
+  // Started to look at this in app reducer, but backed off as wasn't sure about
+  // affecting the ready state if cream data does not start loading until user
+  // visits its page.
+  const creamTokenAddresses = [
+    '0x41c84c0e2EE0b740Cf0d31F63f3B6F627DC6b393',
+    '0x8e595470Ed749b85C6F7669de83EAe304C2ec68F',
+    '0x7589C9E17BCFcE1Ccaa1f921196FDa177F0207Fc',
+  ];
+
+  return (
+    <Wrapper>
+      <table>
+        <thead>
+          <tr>
+            <th>Asset</th>
+            <th>APY</th>
+            <th>Wallet</th>
+            <th>Liquidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {creamTokenAddresses.map(creamTokenAddress => (
+            <CreamBorrowMarketRow
+              key={creamTokenAddress}
+              creamTokenAddress={creamTokenAddress}
+            />
+          ))}
+        </tbody>
+      </table>
+    </Wrapper>
+  );
 };
 
-Cover.whyDidYouRender = true;
-export default Cover;
+Cream.whyDidYouRender = true;
+export default Cream;
--- app/containers/Cream/saga.js
+++ app/containers/Cream/saga.js
@@ -1,6 +1,7 @@
 // import request from 'utils/request';
 import comptrollerAbi from 'abi/creamComptroller.json';
 import priceOracleAbi from 'abi/creamPriceOracle.json';
+import CErc20DelegatorAbi from 'abi/CErc20Delegator.json';
 import { APP_READY } from 'containers/App/constants';
 import {
   COMPTROLLER_ADDRESS,
@@ -103,30 +104,46 @@ function* fetchCreamMarketData() {
   yield put(addContracts(contracts));
 }
 
+function* addCreamCTokens(creamCTokenAddresses) {
+  const contracts = [
+    {
+      namespace: 'creamCTokens',
+      abi: CErc20DelegatorAbi,
+      addresses: creamCTokenAddresses,
+
+      readMethods: [
+        {
+          name: 'borrowRatePerBlock',
+        },
+      ],
+    },
+  ];
+
+  yield put(addContracts(contracts));
+}
+
 function* processCreamMarketDataResponse(action) {
   const { payload } = action;
-  console.log(payload);
 
-  let creamMarkets;
+  const creamComptrollerResult = _.find(payload, [
+    'namespace',
+    'creamComptroller',
+  ]);
+  if (!_.isUndefined(creamComptrollerResult)) {
+    const creamCTokenAddresses = creamComptrollerResult.getAllMarkets;
+    yield addCreamCTokens(creamCTokenAddresses);
+  }
 
-  payload.forEach(batchResult => {
-    if (batchResult.namespace === 'creamComptroller') {
-      console.log(batchResult);
-      creamMarkets = batchResult.getAllMarkets;
-      console.log(creamMarkets);
-    }
+  const creamOracleResult = _.find(payload, ['namespace', 'creamOracle']);
 
-    if (batchResult.namespace === 'creamOracle') {
-      // Not seeing this yet.... not sure why
-      console.log(batchResult);
-    }
-  });
-
-  // Need to add creamMarkets contract next....
+  if (!_.isUndefined(creamOracleResult)) {
+    // Not seeing this yet.... not sure why
+  }
 }
 
 export default function* watchers() {
   yield takeLatest(APP_READY, fetchCreamMarketData);
+  yield takeLatest('BATCH_CALL_RESPONSE', processCreamMarketDataResponse);
   yield takeLatest('BATCH_CALL_RESPONSE', processCreamMarketDataResponse);
 
   // yield takeLatest(COVER_DATA_LOADED, coverDataLoadedSaga);

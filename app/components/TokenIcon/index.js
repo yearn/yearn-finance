c954b0fbde501c9ddc81af6413f3d0566df16fd8
x48 <x48114@protonmail.com>
2020-12-19 17:45:43 -0600

    fix: fix token icon error

--- app/components/TokenIcon/index.js
+++ app/components/TokenIcon/index.js
@@ -4,6 +4,9 @@ import ReactImageFallback from 'react-image-fallback';
 
 export default function TokenIcon(props) {
   const { address, className } = props;
+  if (!address) {
+    return null;
+  }
   const checksumAddress = web3.utils.toChecksumAddress(address);
   const url = `https://raw.githubusercontent.com/iearn-finance/yearn-assets/master/icons/tokens/${checksumAddress}/logo-128.png`;
   const fallbackUrl = `https://i.imgur.com/7lETB36.png`;

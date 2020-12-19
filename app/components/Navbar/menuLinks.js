3c24245271ec678dc6ef31bd5ec4e646af7d8074
Eric Juta <ericjohnjuta@gmail.com>
2020-12-19 22:30:47 +0000

    fix: menu links rearrange, extract out security link to top navbar (#17)
    * fix: update menu links with community dropdown links
    
    * fix: extract out security to top navbar link

--- app/components/Navbar/menuLinks.js
+++ app/components/Navbar/menuLinks.js
@@ -51,14 +51,41 @@ export const menuLinks = {
   //   title: 'Labs',
   //   description: 'Home for experimental vaults',
   // },
-  community: {
-    href: 'https://ycosystem.info/',
-    title: 'Community',
-    description: 'Yearn Community Aggregator',
-  },
+  community: [
+    {
+      href: 'https://twitter.com/iearnfinance',
+      title: 'Twitter',
+      // description: 'Yearn Community Aggregator',
+    },
+    {
+      href: 'http://discord.yearn.finance/',
+      title: 'Discord',
+      // description: 'Yearn Community Aggregator',
+    },
+    {
+      href: 'https://t.me/yearnfinance',
+      title: 'Telegram',
+      // description: 'Yearn Community Aggregator',
+    },
+    {
+      href: 'https://github.com/iearn-finance',
+      title: 'Github',
+      // description: 'Yearn Community Aggregator',
+    },
+    {
+      href: 'https://medium.com/iearn',
+      title: 'Medium',
+      // description: 'Yearn Community Aggregator',
+    },
+  ],
   docs: {
     href: 'https://docs.yearn.finance/',
     title: 'Docs',
     description: 'Yearn finance documentation',
+  },
+  security: {
+    href:
+      'https://github.com/iearn-finance/yearn-protocol/blob/develop/SECURITY.md',
+    title: 'Security',
   },
 };

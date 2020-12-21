9840de35403c80356d442e6fb15d1de8e35a2807
x48 <x48114@protonmail.com>
2020-12-21 00:10:03 -0600

    refactor: turn off whyDidYouRerender unless it's specifically enabled

--- app/wdyr.js
+++ app/wdyr.js
@@ -4,6 +4,6 @@ if (process.env.NODE_ENV === 'development') {
   // eslint-disable-next-line
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
-    trackAllPureComponents: true,
+    trackAllPureComponents: false,
   });
 }

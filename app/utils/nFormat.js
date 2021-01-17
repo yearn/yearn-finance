c51cb5eff74ee3ad5ab3d25a7773dfc68002a5cb
Eric Juta <ericjohnjuta@gmail.com>
2021-01-17 19:41:33 +0000

    fix: nFormat should not be attributed to ram

--- app/utils/nFormat.js
+++ app/utils/nFormat.js
@@ -3,7 +3,7 @@ export const nFormat = num => {
     { value: 1e18, symbol: 'E' },
     { value: 1e15, symbol: 'P' },
     { value: 1e12, symbol: 'T' },
-    { value: 1e9, symbol: 'G' },
+    { value: 1e9, symbol: 'B' },
     { value: 1e6, symbol: 'M' },
     { value: 1e3, symbol: 'k' },
     { value: 1, symbol: '' },

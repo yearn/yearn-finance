7a7b29bb4734b942dab6e97e12f188e9c06a0edb
x48 <x48114@protonmail.com>
2021-01-17 03:12:23 -0600

    refactor: remove dev mode toggle

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 // import ThemeToggle from 'containers/ThemeProvider/toggle';
-import DevModeToggle from 'containers/DevMode';
+// import DevModeToggle from 'containers/DevMode';
 import { Navbar } from 'components/Navbar';
 
-const Toggles = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 20px;
-  > div {
-    margin-right: 20px;
-  }
-`;
+// const Toggles = styled.div`
+//   display: flex;
+//   align-items: center;
+//   margin-left: 20px;
+//   > div {
+//     margin-right: 20px;
+//   }
+// `;
 
 // const StyledConnectButton = styled(ConnectButton)`
 //   margin-right: 15px;
@@ -22,9 +22,6 @@ export default function Header() {
   return (
     <>
       <Navbar />
-      <Toggles>
-        <DevModeToggle />
-      </Toggles>
     </>
   );
 }

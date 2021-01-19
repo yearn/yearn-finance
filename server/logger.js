1ae1dd697414685d88e46c1b2232c848364f6ed1
nymmrx <nymmrx@protonmail.com>
2021-01-19 23:57:01 +0100

    chore: fix build

--- server/logger.js
+++ server/logger.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console */
+/* eslint-disable */
 
 const chalk = require('chalk');
 const ip = require('ip');
@@ -10,7 +10,7 @@ const divider = chalk.gray('\n-----------------------------------');
  */
 const logger = {
   // Called whenever there's an error on the server we want to print
-  error: err => {
+  error: (err) => {
     console.error(chalk.red(err));
   },
 
@@ -26,10 +26,10 @@ const logger = {
     console.log(`
 ${chalk.bold('Access URLs:')}${divider}
 Localhost: ${chalk.magenta(`http://${host}:${port}`)}
-      LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) +
-        (tunnelStarted
-          ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}`
-          : '')}${divider}
+      LAN: ${
+        chalk.magenta(`http://${ip.address()}:${port}`) +
+        (tunnelStarted ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}` : '')
+      }${divider}
 ${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
     `);
   },

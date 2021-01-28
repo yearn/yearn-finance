55194ff47a9986db7f1aab28d7c630cfd1984df5
x48 <x48114@protonmail.com>
2021-01-28 01:09:50 -0600

    refactor: bump version

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -26,7 +26,7 @@ export default function App() {
     }
   };
   const init = () => {
-    console.log('v2.0.3');
+    console.log('v2.0.4');
     dispatch(appInitialized());
   };
   useEffect(appReadyChanged, [ready]);

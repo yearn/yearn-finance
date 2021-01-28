a3235035c9bc3ef38555ddddb0fab690b3156fa4
x48 <x48114@protonmail.com>
2021-01-27 20:58:19 -0600

    refactor: remove unused accounts code

--- app/drizzle/store/rootSaga.js
+++ app/drizzle/store/rootSaga.js
@@ -1,13 +1,5 @@
-import accountsSaga from './accounts/accountsSaga';
-import accountBalancesSaga from './accountBalances/accountBalancesSaga';
 import blocksSaga from './blocks/blocksSaga';
 import contractsSaga from './contracts/contractsSaga';
 import drizzleStatusSaga from './drizzleStatus/drizzleStatusSaga';
 
-export default [
-  accountsSaga,
-  accountBalancesSaga,
-  blocksSaga,
-  contractsSaga,
-  drizzleStatusSaga,
-];
+export default [blocksSaga, contractsSaga, drizzleStatusSaga];

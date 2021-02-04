b3e73d056c10be849234d3ba91eacd1baf3e9314
turtlemoji <78362532+turtlemoji@users.noreply.github.com>
2021-02-04 19:17:52 -0300

    feat: implement backscratcher (#140)
    * feat: added vaultsBackscratchersHeaders
    
    * chore: changed vaultsBackscratcherX name to BackscratcherX
    
    * refactor: using backstratcher as plural
    
    * fix: splashscreen horizontal scroll
    
    * chore: backscratcher mocked data implemented on state
    
    * feat: new backscratcher splash
    
    * chore: unhardcoded stuff
    
    * feat: continue first pass implementation of backscratcher
    
    * fix: new additional info
    
    * fix: backscratcher header alignment and fix
    
    * feat: added link to stake button
    
    * style: added link underline
    
    * fix: hide the vault balance
    
    * fix: hide stake until approve
    
    * fix: fix backscratcher allowance issue
    
    * refactor: remove vault version from backscratcher vault
    
    * fix: backscratcher only in production and fix style
    
    * feat: hardcoded apy
    
    * feat: showing apy on backscratcher description
    
    * fix: stake button
    
    * fix: empty backscratcher
    
    * fix: removed stats from backscratcher vault
    
    * chore: remove console logs
    
    * fix: backscratcher accordion open by default
    
    * fix: button text deposit/approve
    
    * style: new backscratcher splash screen clean media query
    
    * refactor: refactor splash page
    
    Co-authored-by: gotzenx <gotzenlenek@gmail.com>
    Co-authored-by: x48 <x48114@protonmail.com>

---
+++ app/components/SplashScreen/Backscratcher/index.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import 'twin.macro';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import LogoImg from '../../../images/Splash/logo.svg';
+
+const Logo = styled.div`
+  img {
+    width: 60vw;
+    max-width: 270px;
+    opacity: 0.8;
+    margin: 0 auto;
+  }
+`;
+
+const HeaderText = styled.div`
+  font-size: 19px;
+  font-family: 'roboto light';
+  text-align: center;
+  letter-spacing: 2.2px;
+  @media (max-width: 570px) {
+    max-width: 280px;
+  }
+  line-height: 28px;
+  margin-bottom: 10px;
+`;
+
+const Percent = styled.div`
+  display: inline;
+  opacity: 1;
+`;
+
+const Grey = styled.div`
+  display: inline;
+  opacity: 0.5;
+`;
+
+const HeaderTextBig = {
+  fontSize: '40px',
+  letterSpacing: '1.0px',
+  marginBottom: '6px',
+  lineHeight: '43px',
+  marginTop: '30px',
+  opacity: '1',
+};
+
+const buttonLink = {
+  background: '#4F41FF',
+};
+
+const backgroundStyle = `
+  height: calc(100vh - 64px);
+  @media (max-width: 570px) {
+    height: calc(100vh);
+    font-size: 35px;
+  }
+  background-color: #111;
+`;
+
+const imageStyle = `
+  animation: cowmove 4s infinite;
+  @keyframes cowmove{
+      0% {
+        transform: rotate(0deg);
+      }
+      49% {
+        transform: rotate(-20deg);
+      }
+  }
+  opacity: .9;
+`;
+
+export const Backscratcher = () => (
+  <div
+    tw="w-screen relative flex flex-col justify-center items-center overflow-hidden"
+    css={backgroundStyle}
+  >
+    <div tw="flex flex-col justify-center items-center z-10 absolute">
+      <Logo tw="mb-8">
+        <img css={imageStyle} src={LogoImg} alt="logo" />
+      </Logo>
+
+      <div tw="text-white font-bold md:text-3xl justify-center flex flex-col items-center text-2xl">
+        <HeaderText style={HeaderTextBig}>Maximize your CRV rewards</HeaderText>
+      </div>
+      <div tw="text-white font-bold md:text-3xl justify-center flex flex-col items-center mb-6 text-2xl">
+        <HeaderText>
+          <Grey>Earn</Grey> <Percent>38% more</Percent>{' '}
+          <Grey>in weekly fees staking with Yearn</Grey>
+        </HeaderText>
+      </div>
+
+      <Link
+        style={buttonLink}
+        type="button"
+        tw="bg-yearn-blue px-6 rounded-lg py-1.5 flex justify-center items-center align-middle no-underline"
+        to="/vaults"
+      >
+        <p tw="text-white text-base font-sans font-black no-underline">
+          Go to Backscratcher
+        </p>
+      </Link>
+    </div>
+  </div>
+);

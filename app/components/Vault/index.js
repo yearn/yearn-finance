23261dac7a672a3ab278da5107434bc8e93454ce
gotzenx <gotzenlenek@gmail.com>
2021-03-09 14:15:39 -0300

    fix: using token icon for amplify vaults

--- app/components/Vault/index.js
+++ app/components/Vault/index.js
@@ -810,7 +810,7 @@ const Vault = (props) => {
       let amplifyVaultDesc;
       let availableToDeposit = <AnimatedNumber value={tokenBalanceOf} />;
       let styledIcon = (
-        <StyledTokenIcon address={tokenContractAddress} icon={vault.icon} />
+        <StyledTokenIcon address={tokenContractAddress} icon={token.icon} />
       );
       if (vaultIsBackscratcher) {
         amplifyVaultTitle = (

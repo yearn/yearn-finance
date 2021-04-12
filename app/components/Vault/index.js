06d3555f145d230ad8f0e281ebae1541ebd2485b
nymmrx <nymmrx@protonmail.com>
2021-04-12 16:17:09 +0200

    fix(vault): remove hardcoded backscratcher name

--- app/components/Vault/index.js
+++ app/components/Vault/index.js
@@ -415,9 +415,7 @@ const Vault = (props) => {
   // const tokenName = name || _.get(tokenContractData, 'name');
 
   let vaultName;
-  if (vaultIsBackscratcher) {
-    vaultName = 'CRV';
-  } else if (vaultIsPickle) {
+  if (vaultIsPickle) {
     vaultName = 'yveCRV - ETH';
   } else {
     vaultName = displayName || name || address;

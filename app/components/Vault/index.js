06900b3f82c87f688854e542acf1b638d713923c
dudesahn <dudesahn@gmail.com>
2021-05-04 11:24:24 -0400

    feat: enable WBTC v2 migration
    * feat: add WBTC v2 to migration whitelist
    
    * feat: add WBTC v2 to retired list
    
    * feat: update new WBTC to show APY as NEW ✨

--- app/components/Vault/index.js
+++ app/components/Vault/index.js
@@ -782,6 +782,7 @@ const Vault = (props) => {
     '0x07FB4756f67bD46B748b16119E802F1f880fb2CC', // crvTBTC v1
     '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', // ETH v1
     '0xe1237aA7f535b0CC33Fd973D66cBf830354D16c7', // WETH v1
+    '0xcB550A6D4C8e3517A939BC79d0c7093eb7cF56B5', // WBTC v2
   ];
 
   if (address === '0xBA2E7Fed597fd0E3e70f5130BcDbbFE06bB94fe1') {
@@ -797,6 +798,8 @@ const Vault = (props) => {
   } else if (retired.includes(address)) {
     apyRecommended = 'N/A';
     apyTooltip = 'Please migrate funds to v2 to continue earning yield.';
+  } else if (address === '0xA696a63cc78DfFa1a63E9E50587C197387FF6C7E') {
+    apyRecommended = 'NEW ✨';
   }
 
   const contractType = getContractType(vault);

2181aa7e28132bc4a2c32b264aac590ba1a622d9
qq95538 <190808149@qq.com>
2021-06-27 01:19:38 +0800

    fix https://github.com/yearn/yearn-finance/issues/111
    "Max" buttons on vaults/cover should show more than 2 decimals #111
    
    wallet/vault balance  now shows 5 decimals instead of 2 (vaults page)

--- app/components/VaultControls/index.js
+++ app/components/VaultControls/index.js
@@ -1332,7 +1332,7 @@ function MaxButton({ maxAmount, amountSetter, gweiAmountSetter, decimals }) {
       onClick={() => {
         const normalizedAmount = new BigNumber(maxAmount)
           .dividedBy(10 ** decimals)
-          .toFixed(2);
+          .toFixed(5);
 
         amountSetter(normalizedAmount);
         gweiAmountSetter(maxAmount);

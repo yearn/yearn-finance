90ae52c1f973fea27a3871ee46bea9a1ed9c374e
Mikhail <mmvsha73@gmail.com>
2021-07-27 16:14:27 +0200

    feat(wallets): add support for gnosis safe

--- app/containers/ConnectionProvider/services.js
+++ app/containers/ConnectionProvider/services.js
@@ -56,6 +56,7 @@ export function initOnboard(subscriptions, darkMode) {
         { walletName: 'operaTouch' },
         { walletName: 'imToken', rpcUrl },
         { walletName: 'meetone' },
+        { walletName: 'gnosis' },
       ],
     },
     walletCheck: [

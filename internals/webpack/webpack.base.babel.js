2f7ab5f5f0325db83b7088b119abd721f2eb9477
x48 <x48114@protonmail.com>
2021-08-16 09:17:04 -0500

    refactor: update manifest.json

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -10,7 +10,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const ManifestPlugin = new WebpackPwaManifest({
   name: 'yearn.finance',
   short_name: 'yearn.finance',
-  description: 'DeFi made simple',
+  description: 'Put your assets to work.',
   background_color: '#fafafa',
   theme_color: '#b1624d',
   inject: true,

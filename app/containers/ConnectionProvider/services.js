dcb5dd1959b7b46a9224430d18dcd44225919d5d
tima-t <tito.marchev@gmail.com>
2021-08-27 15:03:23 +0300

    feat: enable liquality wallet in ConnectionProvider list (#423)

--- app/containers/ConnectionProvider/services.js
+++ app/containers/ConnectionProvider/services.js
@@ -64,6 +64,7 @@ export function initOnboard(subscriptions, darkMode) {
           rpcUrl,
           appName,
         },
+        { walletName: 'liquality' },
       ],
     },
     walletCheck: [
